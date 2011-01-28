434ac9558d627133bd537fbd15a387ceb9c57636
Andrew Coldham <quasor@me.com>
2011-01-28 10:30:07 -0800

    location

--- lib/instagram.rb
+++ lib/instagram.rb
@@ -10,6 +10,7 @@ module Instagram
   Popular = Addressable::URI.parse 'http://instagr.am/api/v1/feed/popular/'
   UserFeed = Addressable::Template.new 'http://instagr.am/api/v1/feed/user/{user_id}/'
   UserInfo = Addressable::Template.new 'http://instagr.am/api/v1/users/{user_id}/info/'
+  LocationFeed = Addressable::Template.new 'http://instagr.am/api/v1/feed/location/{location_id}/'
   
   def popular(params = {}, options = {})
     parse_response(Popular.dup, params, options.fetch(:parse_with, Timeline))
@@ -17,6 +18,11 @@ module Instagram
   
   def by_user(user_id, params = {}, options = {})
     url = UserFeed.expand :user_id => user_id
+    parse_response(url, params, options.fetch(:parse_with, Timeline))
+  end
+
+  def by_location(location_id, params = {}, options = {})
+    url = LocationFeed.expand :location_id => location_id
     parse_response(url, params, options.fetch(:parse_with, Timeline))
   end
   
@@ -35,7 +41,7 @@ module Instagram
   
   def get_url(url)
     response = Net::HTTP.start(url.host, url.port) { |http|
-      http.get url.request_uri, 'User-agent' => 'Instagram Ruby client'
+      http.get url.request_uri, 'User-agent' => 'Instagram'
     }
     
     if Net::HTTPSuccess === response
